2dbd55baf1c37c2669e8a29271b43f2c817df69a
Nakhaima254 <nakhaimaisaac068gmail.com>
2025-09-06 16:20:31 +0300

    update the files to match local version

---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log(5 == '5')
+
+
+// if (true) {
+//   console.log('hello')
+// }
+// else {
+//   console.log('else');
+// }
+// const age = 15
+
+// if (age >= 16) {
+//   console.log('You can drive')
+// } 
+// else if (age >= 14){
+//   console.log('Almost there')
+// }
+// else{
+//   console.log('you can not drive')
+// }
+
+
+console.log(false && true);
+console.log(0.2 >=0 && 0.2 < 1/3);
